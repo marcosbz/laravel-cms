888629da93aeaa8452f00a5f89987544446f2535
marcosbz <ziegler.marcos.b@gmail.com>
2021-04-14 01:58:07 -0300

    feat: Add sidebar in dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,9 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -29,7 +32,19 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+              <div class="container py-4">
+                <div class="row">
+                  <div class="col-4">
+                    <ul class="list-group">
+                      <li class="list-group-item"><a href="#">Posts</a></li>
+                      <li class="list-group-item"><a href="#">Categories</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-8">
+                    {{ $slot }}
+                  </div>
+                </div>
+              </div>
             </main>
         </div>
     </body>
