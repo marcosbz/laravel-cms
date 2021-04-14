266bbde800a2952a7c690aef8a45dcbec09dfee2
marcosbz <ziegler.marcos.b@gmail.com>
2021-04-14 16:13:03 -0300

    feat: Add CreateCategoryController Request
    Refactor to put validation inside this
    Request object.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateCategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -35,13 +36,8 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateCategoryRequest $request)
     {
-        //
-        $this->validate($request, [
-          'name' => 'required|min:3|unique:categories'
-        ]);
-
         Category::create([
           'name' => $request->name
         ]);
